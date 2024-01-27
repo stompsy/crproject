79cccebd711aebc92eee48438ff294f7fa55b590
Brian Gerdes <brian.gerdes@protonmail.com>
2024-01-27 03:57:01 -0800

    Chore: refactor model form

--- src/agencies/forms.py
+++ src/agencies/forms.py
@@ -18,28 +18,3 @@ class AgencyForm(forms.ModelForm):
         if qs.exists():
             self.add_error("agency_name", f"{agency_name} is already in use.")
         return data
-
-
-class AgencyFormOld(forms.Form):
-    agency_name = forms.CharField()
-    agency_url = forms.URLField()
-    agency_description = forms.CharField(widget=forms.Textarea)
-
-
-# def clean_agency_name(self):
-#     cleaned_data = self.cleaned_data
-#     agency_name = cleaned_data.get("agency_name")
-#     if agency_name.lower().strip() == "Hello":
-#         raise forms.ValidationError("This title is taken.")
-#     return agency_name
-
-
-def clean(self):
-    cleaned_data = self.cleaned_data
-    agency_name = cleaned_data.get("agency_name")
-    agency_url = cleaned_data.get("agency_url")
-    agency_description = cleaned_data.get("agency_description")
-    if agency_name.lower().strip() == "Hello":
-        raise forms.ValidationError("This title is taken.")
-
-    return cleaned_data
